41df1e815bb505bafad6d3d3dfeaecd921ef2049
ansea-wav <prousingood@gmail.com>
2026-06-15 11:51:30 +0700

    chore: Remove embedded AudioEngine and Focus Music widget

--- src/components/MobileEnvironment.tsx
+++ src/components/MobileEnvironment.tsx
@@ -13,7 +13,6 @@ import MobileSettings from './apps/MobileSettings';
 import MobileProtocols, { type ProtocolAppId } from './apps/MobileProtocols';
 import DynamicIsland from './DynamicIsland';
 import NotificationEngine from './NotificationEngine';
-import AudioEngine from './AudioEngine';
 
 // Mobile-specific Apps
 import MobileControlCenter from './apps/mobile/MobileControlCenter';
@@ -205,7 +204,6 @@ export default function MobileEnvironment({ userData, userId }: MobileProps) {
   return (
     <div className="fixed inset-0 bg-[#111113] overflow-hidden text-white flex flex-col">
       <NotificationEngine client={clientRegistry} />
-      <AudioEngine />
       <DynamicIsland />
       {/* Content Area */}
       <div className="flex-1 relative w-full h-full">
